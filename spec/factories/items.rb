d44d0d6207e68d6fbaf3039cde5577ed5d277b53
adpoppo <nisiwow8@gmail.com>
2022-06-30 19:46:40 +0900

    単体テストコードの記述（完了）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,18 @@
 FactoryBot.define do
   factory :item do
-    item_name { Faker::Lorem.word(max_length: 40) }
-    item_description { Faker::Lorem.sentences(max_length: 1000) }
+    after(:build) do |item|
+      item.image.attach(io: File.open('app/assets/images/star.png'), filename: 'star.png')
+    end
+
+    item_name { Faker::Internet.password(max_length: 40) }
+    item_description { Faker::Internet.password(max_length: 1000) }
     category_id { Faker::Number.between(from: 1, to: 10) }
     condition_id { Faker::Number.between(from: 1, to: 6) }
     freight_id { Faker::Number.between(from: 1, to: 3) }
     prefecture_id { Faker::Number.between(from: 1, to: 47) }
     ship_day_id { Faker::Number.between(from: 1, to: 3) }
     price { Faker::Number.between(from: 300, to: 9999999) }
+    
+    association :user
   end
 end
