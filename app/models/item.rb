f104a7fd0dd72a78061f07fdbf8b18da4788dd90
adpoppo <nisiwow8@gmail.com>
2022-06-30 12:18:45 +0900

    ActiveHash導入と各モデル作成、及びバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :freight
+  belongs_to :prefecture
+  belongs_to :ship_day
   has_one_attached :image
+
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_description
+    validates :price, numericality: { only_integer: true, message: "is invalid.Input half-width characters." }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range." }
+  end
+
+  with_options numericality: { other_than: 0, message: "can't be blank" } do
+    validates :category_id
+    validates :condition_id
+    validates :freight_id
+    validates :prefecture_id
+    validates :ship_day_id
+  end
 
 end
