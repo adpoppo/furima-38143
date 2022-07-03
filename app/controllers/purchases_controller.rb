efdd64cee87dc05baa9355a640b700e629573460
adpoppo <nisiwow8@gmail.com>
2022-07-03 13:39:51 +0900

    フォーム情報保存処理の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,12 +1,28 @@
 class PurchasesController < ApplicationController
 
   def index
-    @item = Item.find(params[:item.id])
+    @item = Item.find(params[:item_id])
     @purchase_customer = PurchaseCustomer.new
   end
 
   def create
-    
+    @purchase_customer = PurchaseCustomer.new(@purchase_customer_params)
+    if @purchase_customer.valid?
+      @purchase_customer.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+
+  end
+
+  private
+
+    def purchase_customer_params
+      params.require(:purchase_customer).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(:user_id: current_user.id, item_id: params[:item_id].to_i)
+    end
+
   end
 
 end
--- app/models/purchase_customer.rb
+++ app/models/purchase_customer.rb
@@ -1,5 +1,5 @@
 class PurchaseCustomer
-  include Activemodel::Model
+  include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :purchase_id
 
   with_options presence: true do
@@ -11,12 +11,12 @@ class PurchaseCustomer
     validates :house_number
     validates :purchase_id
   end
-  validates :phone_number presence: true
-  validates :phone_number format: { with: /\A\d{10,11}\z/, message: 'is too short.' }
-  validates :phone_number numericality: { only_integer: true, message: 'is invalid. Input only number.' }
+  validates :phone_number, presence: true
+  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is too short.' }
+  validates :phone_number, numericality: { only_integer: true, message: 'is invalid. Input only number.' }
   
   def save
-    purchase = Purchase.create(user_id: current_user.id, item_id: params[:item.id])
+    purchase = Purchase.create(user_id: current_user.id, item_id: params[:item_id].to_i)
     Customer.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, purchase_id: purchase_id)
   end
 end
