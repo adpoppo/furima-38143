983e649c684a4e23d6321183a61c86ff20ef4a01
adpoppo <nisiwow8@gmail.com>
2022-07-04 18:37:53 +0900

    sold out表示の条件分岐処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -37,7 +37,7 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless user_signed_in? && current_user.id != @item.user_id
+    redirect_to root_path unless user_signed_in? && current_user.id != @item.user_id && @item.purchase == nil
   end
 
 end
