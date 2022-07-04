e0cc72529a4bbf29c00dbdc9e205658a332edcbe
adpoppo <nisiwow8@gmail.com>
2022-07-04 21:40:48 +0900

    修正

--- app/models/purchase_customer.rb
+++ app/models/purchase_customer.rb
@@ -1,6 +1,6 @@
 class PurchaseCustomer
   include ActiveModel::Model
-  attr_accessor :token, :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,
+  attr_accessor :token, :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number
 
   with_options presence: true do
     validates :token
