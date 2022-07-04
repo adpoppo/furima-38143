856737d64b9f1be7163e4ac0791fa99925a74249
adpoppo <nisiwow8@gmail.com>
2022-07-04 19:46:51 +0900

    商品購入機能の修正

--- app/models/purchase_customer.rb
+++ app/models/purchase_customer.rb
@@ -1,7 +1,6 @@
 class PurchaseCustomer
   include ActiveModel::Model
   attr_accessor :token, :user_id, :item_id, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number,
-                :purchase_id
 
   with_options presence: true do
     validates :token
